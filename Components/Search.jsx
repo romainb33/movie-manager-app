bc605c18de2cb289e04aaeedf89dcdc63155a48a
Romain B <romain.bnd@gmail.com>
2021-07-21 15:23:27 +0200

    add film component + render data

--- Components/Search.jsx
+++ Components/Search.jsx
@@ -1,30 +1,38 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Button, TextInput } from 'react-native'
+import React, { Component } from "react";
+import { StyleSheet, View, Button, TextInput, FlatList } from "react-native";
+import FilmItem from "./FilmItem";
+
+import films from "../Helpers/filmData";
 
 export class Search extends Component {
-    render() {
-        return (
-            <View style={styles.main_container}>
-                <TextInput style={styles.textinput} placeholder="Titre du film"/>
-                <Button title="rechercher" onPress={() => {}}/>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <TextInput style={styles.textinput} placeholder="Titre du film" />
+        <Button title="rechercher" onPress={() => {}} />
+        <FlatList
+          data={films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <FilmItem film={item} />}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        flex: 1,
-        marginTop: 20
-    },
-    textinput: {
-      marginLeft: 5,
-      marginRight: 5,
-      height: 50,
-      borderColor: '#000000',
-      borderWidth: 1,
-      paddingLeft: 5
-    }
-  });
+  main_container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: "#000000",
+    borderWidth: 1,
+    paddingLeft: 5,
+  },
+});
 
-export default Search
+export default Search;
